4aa6c5a9901af83c21e44bc30767676bb9b963bb
呂芯萍 <aka_lu@bankpro.com.tw>
2019-08-30 10:34:28 +0800

    For build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
+  postTitles: [
+    {
+      id: 0,
+      title: 'akaSchool',
+      path: ''
+    },
+    {
+      id: 1,
+      title: '從零開始的爆肝工程師日常',
+      path: 'helloWorld'
+    },
+    {
+      id: 2,
+      title: '從零開始的個人網站【Angular、Git、GitHub 基礎入門】',
+      path: 'helloWebsite'
+    }
+  ],
   production: true
 };
