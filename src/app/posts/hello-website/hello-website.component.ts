b1219d3b7131abeb97eeeb930490e7ea0753a274
呂芯萍 <aka_lu@bankpro.com.tw>
2019-08-30 10:31:51 +0800

    Create post: helloWebsite

---
+++ src/app/posts/hello-website/hello-website.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-website',
+  templateUrl: './hello-website.component.html',
+  styleUrls: ['./hello-website.component.css']
+})
+export class HelloWebsiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
