097e495dca2b6dcb9369931d154af379d92bfcfd
呂芯萍 <aka_lu@bankpro.com.tw>
2021-01-21 17:11:19 +0800

    Add viewCount into footer & footer padding top, left, right(10px)

--- src/app/post-layout/footer/footer.component.ts
+++ src/app/post-layout/footer/footer.component.ts
@@ -1,15 +1,46 @@
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Component, OnInit } from '@angular/core';
+import { finalize, map } from 'rxjs/operators';
+class Visitors {
+  ViewCount: number;
+  UpdDate: Date;
+}
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"],
 })
 export class FooterComponent implements OnInit {
+  public visitors: Visitors;
 
-  constructor() { }
+  constructor(private afs:AngularFirestore) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.visitors =  await this.getViewCount();
+    this.visitors.ViewCount = this.visitors.ViewCount? this.visitors.ViewCount + 1 : 1;
+    this.visitors.UpdDate = new Date();
+    this.updateViewCount();
   }
 
+  getViewCount():Promise<Visitors>{
+    return new Promise((resolve) => {
+      this.afs
+        .doc<Visitors>("/SystemCode/Visitors")
+        .snapshotChanges()
+        .pipe(map((v) => v.payload.data()))
+        .subscribe(
+          (v: Visitors) => {
+            resolve(v);
+          },
+          (err) => {
+            console.log("err...");
+            // console.log(err);
+          }
+        );
+    });
+  }
+  updateViewCount(){
+    this.afs.doc<Visitors>("SystemCode/Visitors").update(this.visitors);
+  }
 }
